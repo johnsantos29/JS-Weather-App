999851eb0052c224fd7abc6d34f3e6741076b68a
John Erick Santos <johnsantosdev@gmail.com>
2023-04-09 15:34:59 +1000

    Initial Commit

---
+++ JS-Weather-App/JS_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JS_Weather_AppApp.swift
+//  JS-Weather-App
+//
+//  Created by John Erick Santos on 9/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct JS_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
